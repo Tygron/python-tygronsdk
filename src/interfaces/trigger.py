b2c6acd1a9cb4873733f83bfc2fde849fce6ed31
rudolf <rudolf@tygron.com>
2022-02-07 12:48:37 +0100

    Standardized trigger call

--- src/interfaces/trigger.py
+++ src/interfaces/trigger.py
@@ -1,27 +1,36 @@
 from ..core import sdk as tygron
 
+import sys
+
 from collections import OrderedDict
 from typing import Union
+import json
 
 class Trigger():
 
-    def exec( cls ):
-    
+    @classmethod
+    def exec(cls, args:list = None):
+        if (args == None):
+            args = sys.argv;
+
         if len(sys.argv) <= 3:
-            raise IndexError('A trigger, a host, and an api token must be provided as arguments')
+            raise IndexError('A host, and an api token must be provided as arguments. The trigger\'s file is the implicit first parameter. A parameter argument is optional.')
             
-            
-        trigger     = sys.argv[1]
-        host        = sys.argv[2]
-        apitoken    = sys.argv[3]
-        
-        if not len(sys.argv) <= 4:
-            parameters  = sys.argv[4]
+        trigger     = args[0]
+        host        = args[1]
+        apitoken    = args[2]
+        parameters  = {}        
 
+        if not len(args) <= 3:
+            parameters  = json.loads(args[3])
 
-        #trigger_object = new cls(host, )
-    
-        return cls(name, date.today().year - year)        
+        #print([host, apitoken, parameters])    
+
+        trigger_object = cls(host, apitoken, parameters)
+        trigger_object.trigger()  
+
+        return trigger_object
+
 
     def run( self ):
         raise NotImplementedError('Run method of trigger '+self.__class__.__name__+' is not yet implemented.')
@@ -51,21 +60,33 @@ class Trigger():
     def add_results( self, results:dict = {}, replace:bool = False ):
         for key, result in results.items():
             self.add_result(key, result, replace)
- 
+    
+    def get_exception( self ):
+        return self._exception
+
+    def set_exception( self, exception:Exception ):
+        self._exception = exception
+
+    def get_results_structure( self ):
+        return {
+            'TQL' : self.get_results(),
+            'EVENTS' : self.get_results(),
+            'exception' : str(self.get_exception()) if self.get_exception() else None,
+        }
+
     def __init__( self, host: str, apitoken: str = None, parameters: dict = {} ):
         self._parameters = parameters
         self._results = {}
-        
+        self._exception = None        
+
         self._sdk = tygron.sdk({
                 'host': host
             })
         self._session_connection = self._sdk.create_connector_session()
         self._session_connection.set_api_token(apitoken)
-
-
-        
+    
     def trigger( self ):
-        
-        self.run()
-        
-        return self._results
+        try:
+            self.run()
+        except Exception as e:
+            self.set_exception(e)
