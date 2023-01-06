486a8114296ce3a9cf293368df53d9797338a158
rudolf <rudolf@tygron.com>
2023-01-06 17:00:27 +0100

    Improved Exception stringification

--- src/utilities/exceptions.py
+++ src/utilities/exceptions.py
@@ -5,23 +5,18 @@ import os, traceback
 class Exceptions:
 
     @staticmethod
-    def stringify( exception:Exception ):
+    def stringify( exception:Exception,  ):
         string = ''
-        try:
-            raise exception
-        except Exception as e:
-            for value in exception.args:
-                if ( isinstance(value, Exception) ):
-                    #TODO: Traces are printed a bit oddly now, but usable
-                    string += Exceptions.stringify(value)
-                else:
-                    string += str(value)
-                string += os.linesep
-            tb = traceback.format_exc()
-            string += tb
-            string += os.linesep
-            return string
-        return None
+        
+        for value in exception.args:
+            if ( isinstance(value, Exception) ):
+                string += Exceptions.stringify(value)
+                
+        tb = traceback.format_exception(exception)
+        string += ''.join(tb)
+        
+        string += os.linesep
+        return string
 
 
 
