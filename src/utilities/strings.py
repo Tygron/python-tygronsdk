c7ace1c78dca904b7a91f5d9c1587cf56ea67d79
rudolf <rudolf@tygron.com>
2023-01-26 11:17:33 +0100

    Formatting function allowing for retention of unused terms

--- src/utilities/strings.py
+++ src/utilities/strings.py
@@ -18,3 +18,31 @@ class Strings:
             if ( len(string) > max_length ):
                 return False
         return True
+        
+    @staticmethod
+    def format( string:str = '', **kwargs ):
+        formatted = ''
+        to_process = string
+        while( True ):
+            start_index = to_process.find('{') 
+            end_index = to_process.find('}', start_index)
+            if ( (start_index < 0) or (end_index < 0) ):
+                formatted += to_process
+                break
+            
+            formatted += to_process[:start_index]
+            formattable = to_process[start_index:end_index+1]
+            to_process = to_process[end_index+1:]
+            
+            try:
+                formatted += formattable.format(**kwargs)
+            except:
+                formatted+=formattable
+        return formatted
+            
+        """
+            Find start of format keys
+            Find end of format key
+            Substring and attempt to replace it with formatted
+            If error, replace with substring itself
+        """
