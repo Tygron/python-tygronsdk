bef892cea31b11e6eb6091f4055d272999edefd3
rudolf <rudolf@tygron.com>
2023-01-26 11:14:15 +0100

    Allowed datetime manipulations to use timestamps

--- src/utilities/datetimes.py
+++ src/utilities/datetimes.py
@@ -1,25 +1,31 @@
 from datetime import datetime
 
+from typing import Union
 
 class Datetimes:
     
+    @staticmethod
     def guess_is_timestamp_in_milliseconds( ts:int ):
         return ( ts > 10000000000 )
     
     @staticmethod
-    def datetime_to_string_datetime( dt:datetime ):
+    def datetime_to_string_datetime( dt:Union[int,datetime] ):
+        dt = Datetimes.timestamp_to_datetime(dt)
         return Datetimes.datetime_to_string(dt, '%Y-%m-%d %H:%M:%S')
 
     @staticmethod
-    def datetime_to_string_date( dt:datetime ):
+    def datetime_to_string_date( dt:Union[int,datetime] ):
+        dt = Datetimes.timestamp_to_datetime(dt)
         return Datetimes.datetime_to_string(dt, '%Y-%m-%d')
         
     @staticmethod
-    def datetime_to_string_time( dt:datetime ):
+    def datetime_to_string_time( dt:Union[int,datetime] ):
+        dt = Datetimes.timestamp_to_datetime(dt)
         return Datetimes.datetime_to_string(dt, '%H:%M:%S')
         
     @staticmethod
-    def datetime_to_string( dt:datetime, format:str = '%Y-%m-%d %H:%M:%S' ):
+    def datetime_to_string( dt:Union[int,datetime], format:str = '%Y-%m-%d %H:%M:%S' ):
+        dt = Datetimes.timestamp_to_datetime(dt)
         if ( dt == None ):
             return None
         return dt.strftime(format)
@@ -27,8 +33,10 @@ class Datetimes:
         
         
     @staticmethod
-    def timestamp_to_datetime( ts:int, millis:bool = None ):
-        if ( ts == None ):
+    def timestamp_to_datetime( ts:Union[int,datetime], millis:bool = None ):
+        if ( isinstance(ts, datetime) ):
+            return ts
+        if ( ts is None ):
             return None
         if ( millis is None ):
             millis = Datetimes.guess_is_timestamp_in_milliseconds(ts)
