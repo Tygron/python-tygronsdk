1b64b769436e307c03c9b5d56b055f15db6d48f4
rudolf <rudolf@tygron.com>
2023-01-30 15:51:01 +0100

    Added function to root of sdk for ease-of-use obtaining credentials for Tygron

--- __init__.py
+++ __init__.py
@@ -7,8 +7,22 @@ from .src import utilities as utilities
 
 from .src.environments.session.data import items as items
 
+
 events = core.events.EventSetCollection(**{
     **utilities.modules.get_content_from_module(getattr(environments.base.data,'events',[]), core.events.EventSet),
     **utilities.modules.get_content_from_module(getattr(environments.session.data,'events',[]), core.events.EventSet),
     **utilities.modules.get_content_from_module(getattr(environments.share.data,'events',[]), core.events.EventSet),
 })
+
+
+def load_credentials_from_file( file:str = None, files:list=[] ):
+    for f in ([ *files, file ]):
+        try :
+            if ( f is None ):
+                continue
+            credentials = utilities.credentials.CredentialsTygron( file=f )
+            return credentials
+        except Exception as err:
+            error = err
+            pass
+    raise error
