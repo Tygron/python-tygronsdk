bfef16d95ddb47a2ee750de2c0e0b67f23359dbb
rudolf <rudolf@tygron.com>
2023-02-01 15:43:04 +0100

    Added example script for running input generator and orchestrator

---
+++ examples/automations/template_runner_orchestrator.py
@@ -0,0 +1,110 @@
+import tygronsdk
+from tygronsdk import sdk as tygron
+from tygronsdk import utilities as utilities
+from tygronsdk import interfaces as interfaces
+from tygronsdk.implementations import automations
+
+
+
+
+
+
+import os
+from pathlib import Path
+
+def main():
+
+    try:
+        credentials = tygronsdk.load_credentials_from_file( files=[
+                './credentials.txt',
+                './credentials.json'
+            ] )
+    except:
+        print('Credentials must be provided, defining "username" and "password". Can either be a json object in "credentials.json", or key-value pairs in "credentials.txt".')
+        return
+        
+    print( 'This example will demonstrate how the TemplateRunnerOrchestrator can be used to automatically perform repeated calculations.' )
+    
+    
+    
+    print( os.linesep, 'Step 1 : Creation of workspace' )
+    
+    print( 'For the example, an example workspace will be created with a folder to store template data.')
+
+    base_directory = 'tygron_example_outputs/automations_workspace/'
+    print( 'This base directory will be: '+base_directory)
+    
+
+    print( 'At this time, it is important to know about the directories this tooling uses:' )
+    
+    template_dir =  os.path.join(base_directory,    'template' )
+    data_dir =      os.path.join(base_directory,    'data'     )
+    input_dir =     os.path.join(base_directory,    'input'    )
+    running_dir =   os.path.join(base_directory,    'running'  )
+    output_dir =    os.path.join(base_directory,    'output'   )
+    error_dir =     os.path.join(output_dir    ,    'error'    )
+    
+    print( '    Template: '+template_dir+' , which holds the templates for task files, out of the way from actual automations.' )
+    print( '    Data    : '+data_dir+' , which holds additional data, such as geojsons for individual task files, so it can be accessed during runs but is otherwise out of the way from actual automations.' )
+    print( '    Input   : '+input_dir+' , which hold the individual task files which have not yet run.' )
+    print( '    Running : '+running_dir+' , which are associated with an active TemplateRunner and are being processed at that time.' )
+    print( '    Output  : '+output_dir+', which holds both the completed tasks as well as (by default) the exported results and logs.' )
+    print( '    Error   : '+error_dir+', which is a subdirectory of output and will hold the tasks and logs of any runs that encountered an error, and should be investigated and/or rerun.' )
+
+    
+    print( 'In the template folder 2 template files are required: A Geojson indicating locations, and a json file defining the template for all tasks' )
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    example_files_dir = os.path.join( current_dir, 'example_files' )
+    
+    utilities.files.copy_file( 'template_input.json'    , example_files_dir, template_dir, overwrite=True )
+    utilities.files.copy_file( 'locations.geojson'      , example_files_dir, template_dir, overwrite=True )
+
+    print( 'Note that each of these directories and files can be renamed through settings provided to the tooling, if neccesary.')   
+    
+    
+    
+    print( os.linesep, 'Step 2 : Input generator' )
+    
+    print( 'The template.json file serves as a template based on which to create multiple individual task files. The locations.geojson file defines the amount of task files, and the locations of the projects.' )
+    print(' The TemplateRunnerInputGenerator will take the files as input, and generate the required output files.')
+    
+    input_generator = automations.TemplateRunnerInputGenerator({
+            'input_file' : 'template_input.json',
+            'geojson_file' : 'locations.geojson',
+            'base_directory' : base_directory
+        }) # InputGenerator allows more settings, and can run without any settings as well.
+    
+    input_generator.run()
+    
+    for directory in [ template_dir, input_dir, data_dir ]:
+        print( 'The following files now exist in {dir}: {list}'.format(
+                dir=directory,
+                list=( ', '.join(utilities.files.get_content_of_directory(directory)) )
+            ))
+    
+    
+    
+    print( os.linesep, 'Step 3 : Orchestrator' )
+    
+    print( 'Each individual task file is intended to compatible with the TemplateRunner.' )
+    print( 'The TemplateRunnerOrchestrator will manage creating TemplateRunners based on the task files at an appropriate rate.' )
+    
+    orchestrator = automations.TemplateRunnerOrchestrator({
+            'on_start_clean_output' : True,
+            'domain_projects_limit_fraction' : 0.5,
+            'base_directory' : base_directory,
+            'default_credentials_file' : credentials.source
+        }) # Orchestrator allows more settings, and can run without any settings as well.
+    
+    orchestrator.run()
+    
+    print( 'After the orchestrator has completed all tasks, all logging from the orchestrator can be found on the terminal output.' )
+    print( 'In addition, logs are also output to the output directory found at :'+input_dir )
+
+    for directory in [ input_dir, output_dir, error_dir ]:
+        print( 'The following files now exist in {dir}: {list}'.format(
+                dir=directory,
+                list=( ', '.join(utilities.files.get_content_of_directory(directory)) )
+            ))
+            
+main()
