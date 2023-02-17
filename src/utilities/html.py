9ae21c4492b37d048e3273326805f1719b97e2da
rudolf <rudolf@tygron.com>
2023-02-17 16:47:24 +0100

    Improved parsing of html tables, filling up missing spots and working around slightly invalid html

--- src/utilities/html.py
+++ src/utilities/html.py
@@ -1,9 +1,12 @@
 from html.parser import HTMLParser
+from .lists import Lists
 
 class Html:
     
     @staticmethod
-    def parse_html_table_to_list( html:str, vertical_first:bool=False ):
+    def parse_html_table_to_list( html:str, vertical_first:bool=False, fill_up:bool = True ):
+        html = html if (not (html is None)) else ''
+    
         output = []
         
         class MyHTMLParser(HTMLParser):
@@ -12,17 +15,22 @@ class Html:
             cell = None
             def handle_starttag(self, tag, attrs):
                 if ( tag == 'tr' ):
+                    # Catch for unclosed row
+                    self.handle_endtag('td')
+                    self.handle_endtag('tr')
                     self.row = []
                 if ( tag == 'td' or tag == 'th' ):
+                    self.handle_endtag('td')
                     self.cell = ''
 
             def handle_endtag(self, tag):
                 if ( tag == 'td' or tag == 'th'):
-                    if ( isinstance(self.row,list) ):
+                    if ( isinstance(self.row,list) and ( not (self.cell is None) ) ):
                         self.row.append(self.cell)
                     self.cell=None
                 if ( tag == 'tr' ):
-                    self.output.append(self.row)
+                    if ( isinstance(self.row,list) ):
+                        self.output.append(self.row)
                     self.row = None
 
             def handle_data(self, data):
@@ -33,9 +41,11 @@ class Html:
         parser.feed(html)
         output = parser.output
         
+        if ( fill_up ):
+            output = Lists.list_of_lists_fill_up( output )
+            
         if ( vertical_first ):
-            output = list( zip( *output ) )
-        
+            output = [ list(el) for el in zip( *output, strict=True ) ]
         return output
         
         
@@ -44,7 +54,8 @@ class Html:
         result_dict = {}
         parsed_list = Html.parse_html_table_to_list(
                 html=html,
-                vertical_first=vertical_first
+                vertical_first=vertical_first,
+                fill_up=True
             )
         headers = None
         for row in parsed_list:
@@ -56,7 +67,6 @@ class Html:
                 for head in row:
                     headers.append( key_mapping.get(head.lower(), head) )
                 continue
-            
             row_key = str(row[0])
             row_key = key_mapping.get(row_key.lower(), row_key)
             result_dict[row_key] = {}
