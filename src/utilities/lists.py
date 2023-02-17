e5493b45b11a4d8066771bde0c9561139a8270c3
rudolf <rudolf@tygron.com>
2023-02-17 16:46:16 +0100

    Utilities for processing information retrieved in list-form

--- src/utilities/lists.py
+++ src/utilities/lists.py
@@ -1,6 +1,4 @@
-
-
-from typing import List
+from typing import Union, List
 
 class Lists:
     
@@ -15,4 +13,29 @@ class Lists:
         if ( (len(value) > index) and (not value[index] == None) ):
             return value[index]
         return default
+    
+    @staticmethod
+    def list_of_lists_fill_up( list_of_lists:list = [], filler=None):
+        new_list_of_lists = [l[:] for l in list_of_lists]
+        max_len = max( [len(l) for l in new_list_of_lists] )
+        for l in new_list_of_lists:
+            l.extend( filler for _ in range( max_len - len(l) )  )
+        return new_list_of_lists
+            
+    
+    @staticmethod
+    def list_of_lists_to_dict( list_of_lists:list = [], indexes:Union[int,list] = 0, keys:list = None ):
+        output = {}
         
+        list_of_lists = Lists.list_of_lists_fill_up(list_of_lists)
+        
+        for index, l in enumerate(list_of_lists):
+            key = indexes[index] if isinstance(indexes, list) else l[indexes]
+            
+            if ( keys is None ):                    
+                output[key] = l
+                continue
+            output[key] = {}
+            for sub_index, sub_key in enumerate(keys):
+                output[key][sub_key] = l[sub_index]
+        return output
