3181688b8a3ccf32da62aab868d75a3e228518bd
rudolf <rudolf@tygron.com>
2023-02-28 12:29:38 +0100

    Further improements to facilitate support of None definitions for events, excluding their use explicitly

--- src/core/events/event_set.py
+++ src/core/events/event_set.py
@@ -24,19 +24,24 @@ class EventSet():
     def definitions( self ):
         return self._definitions
         
-    def get_definitions_as_grouped_list( self ):
+    def get_definition_names_as_grouped_list( self, include_empty:bool = False ):
         definitions = []
         for group_name, group_list in self.groups.items():
             for def_name in group_list:
-                definitions.append(self._definitions[def_name])
+                if ( (not(self._definitions[def_name] is None)) or include_empty ):
+                    definitions.append(def_name)
         return definitions
-        
+    
+    def get_definition( self, name ):
+        definition = self._definitions.get( name , None )
+        return definition
+    
     def get_event( self, name ):
         name = str(name).lower()
         domain = str(self.domain).lower()
         event_path = Event.generate_path(domain, name)
         
-        definition = self._definitions.get( name , None )
+        definition = self.get_definition( name )
         
         if ( definition is None ):
             raise Exception( 'No event known as ' + event_path )
@@ -46,22 +51,21 @@ class EventSet():
             event_name = name,
             parameters = definition if isinstance(defintion,list) else definition.parameters
         )
-        
-        try:
-            definition
-        except Exception as err:
-            raise Exception('An exception while trying to retrieve event definition '+event_path+': '+str(err))
-            
 
     def _generate_definitions( self, definitions:dict = {} ):
         for event_definition_name, event_definition in definitions.items():
-            self._store_definition( event_definition )
-            setattr(self, event_definition_name, event_definition )
+            self._store_definition( name=event_definition_name, definition=event_definition )
+            if ( not ( event_definition is None ) ):
+                setattr(self, event_definition_name, event_definition )
             
-    def _store_definition( self, definition:EventDefinition ):
-        self._definitions[definition.name] = definition
-        self.groups[definition.group] = self.groups.get(definition.group, [])
-        self.groups[definition.group].append(definition.name)
+    def _store_definition( self, name:str = None, definition:EventDefinition = None ):
+        group = None if (definition is None) else definition.group
+        name = name if (definition is None) else definition.name
+        
+        self._definitions[name] = definition
+            
+        self.groups[group] = self.groups.get(group, [])
+        self.groups[group].append(name)
         
             
     def _normalize_definitions( self, event_definitions:Union[dict,list]=[] ):
@@ -70,8 +74,16 @@ class EventSet():
             event_definitions = { k:v for k, v in enumerate(event_definitions) }
          
         for i, definition in event_definitions.items():
+            if (definition is None):
+                defs[i] = None
+                continue
+        
             if ( isinstance(definition, dict) ):
                 definition = EventDefinition(**definition)
+                
+            if ( not isinstance(definition, EventDefinition) ):
+                continue
+                
             if ( definition.name is None ):
                 definition.name = i
             if ( definition.domain is None ):
